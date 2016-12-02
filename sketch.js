ba759f3a974a1a54db9f82bd3c497cb4b4d374cd
Radhika Mattoo <rm3485@nyu.edu>
2016-12-02 12:32:53 -0500

    added alien dae that we could possibly use. Fiddling with distance threshold and trying to figure out how to properly prompt the user if they want to land or not. I've emailed craig asking for some advice on this

--- sketch.js
+++ sketch.js
@@ -1,3 +1,18 @@
+/**
+ * Space VR Explorer 
+ * Maz & Radhika 
+ * 
+ * 
+ * /
+
+/* TODO: 
+ * Prompting user if they want to 'land' on a planet (should be implemented in the promptUser function)
+ * Loading 'alien' DAE model when you land on a planet. I guess if you get close enough to the alien you leave the planet 
+ * Border recognition for leaving a planet - when the user exits the plane, the plane should revert back to a planet. 
+ * Equirectangular image wraparound
+ * 
+ */
+
 // variable to hold a reference to our A-Frame world
 var world;
 var acceleration = 0.01;
@@ -10,9 +25,8 @@ var ship;
 var gameState = 0;
 
 function setup() {
-
 	// no canvas needed
-	noCanvas();
+	createCanvas(1000,1000);
 
 	// construct the A-Frame world
 	// this function requires a reference to the ID of the 'a-scene' tag in our HTML document
@@ -21,8 +35,6 @@ function setup() {
 	world.setFlying(true);
 
 	for (var i = 0; i < 50; i++) {
-
-
 		// pick a location
 		var x = random(-random(2000), random(2000));
 		var y = random(-random(2000),random(2000));
@@ -35,46 +47,39 @@ function setup() {
 		var container = new Container3D({x:x, y:y, z:z});
 		var planet = new Planet(x,y,z,r,g,b);
 
-	// add the container to the world
-	world.add(planet.container);
-	
-	
-		var b = new Sphere({
-							x:x,
-							y:y,
-							z:z,
-							radius: 20,
-							
-							red: r, green: g, blue: b,
-						/*
-							clickFunction: function(theBox) {
-								// update color
-
-
-								// move the user toward this box over a 2 second period
-								// (time is expressed in milliseconds)
-								world.slideToObject( theBox, 2000 );
-							}
-							*/
-		});
-
-        if(i <10){
-          var myDAE = new DAE({asset:'model1', x:x+20, y:y+20,z:z+20});
-		  	
-		  	
-		  	planet.addLife('model1');
-          
-        }
-		  	
-		  	planet.addObj(b);
-		  	
-		  	
-	planets.push(planet);
-		// add the box to the world
-		//world.add(b);
-	}
-
-  ship = new Ship()
+  	// add the container to the world
+  	world.add(planet.container);
+  
+  	var b = new Sphere({
+  							x:x,
+  							y:y,
+  							z:z,
+  							radius: 20,
+  							
+  							red: r, green: g, blue: b,
+  						/*
+  							clickFunction: function(theBox) {
+  								// update color
+  
+  
+  								// move the user toward this box over a 2 second period
+  								// (time is expressed in milliseconds)
+  								world.slideToObject( theBox, 2000 );
+  							}
+  							*/
+  		});
+  
+      if(i <15){
+        var myDAE = new DAE({asset:'model1', x:x+20, y:y+20,z:z+20});
+    	  planet.addLife('model1');
+      }
+    	planet.addObj(b);
+    	planets.push(planet);
+    		// add the box to the world
+    		//world.add(b);
+    	}
+  
+    ship = new Ship();
 }
 
 
@@ -126,7 +131,7 @@ function Ship(){
 		//var pos = world.getUserPosition();
 		//world.setUserPosition(pos.x,pos.y+1,pos.z)
 	  }
-	world.moveUserForward(this.velocity);
+	  world.moveUserForward(this.velocity);
   }
   
   
@@ -138,7 +143,7 @@ function Planet(xPos,yPos,zPos,r,g,b){
   this.z = zPos;
   this.inhabitants = [];
   this.containsLife = false;
-  this.distanceToShip;
+  this.onPlanet = false; 
   
   this.r = r;
   this.g = g;
@@ -164,14 +169,16 @@ function Planet(xPos,yPos,zPos,r,g,b){
   this.addLife = function(modelName){
     var myDAE = new DAE({asset:modelName, x:xPos+30, y:yPos+30,z:zPos+30});
     this.inhabitants.push(myDAE);
-		  	this.container.addChild(myDAE);
+		this.container.addChild(myDAE);
+		this.containsLife = true; 
     
   }
   
   this.generateWorld = function(){
     //this.world = new World('VRScene');
+    console.log("SETTING UP WORLD");
     world.setFlying(false);
-  var g = new Plane({
+    var g = new Plane({
 						x:this.x, y:this.y, z:this.z, 
 						width:this.radius*40, height:this.radius*40,
 						red:this.r,green:this.g,blue:this.b,
@@ -180,21 +187,24 @@ function Planet(xPos,yPos,zPos,r,g,b){
 						repeatY: 100,
 						rotationX:-90
 					   });
-  world.add(g);
+    world.add(g);
+
 }
  
- //Code to check if the user is close enough to enter a planet 
+  //Code to check if the user is close enough to enter a planet 
   this.calculateDistance = function(){
     var pos = world.getUserPosition();
     var distance = dist(this.x,this.y,this.z,pos.x,pos.y,pos.z)
     
     this.distance = distance;
     
-    if(this.distance < 25 && world.getFlying()){
-      console.log("world")
-      this.generateWorld();
-        //gameState = 1;
+    if(this.distance < 200 && world.getFlying()){
+      if(this.containsLife){
+        console.log("NEAR PLANET");
         ship.velocity = 0;
+        promptUser();
+        this.generateWorld();
+      }
     }
     
   }
@@ -202,7 +212,15 @@ function Planet(xPos,yPos,zPos,r,g,b){
   this.spin = function(){
     this.inhabitants[0].spinY(1);
   }
-  
+}
+
+
+//prompt the user and ask them if they want to 'land' on the planet or not 
+//right now, they land whether or not they want to 
+function promptUser(){
+  console.log("prompting user");
+  textSize(20);
+  text("Land?", 500,500);
   
 }
 
